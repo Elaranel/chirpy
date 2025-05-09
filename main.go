1d5c3e08d7451d9342eda01f67e4f8870ef99963
Elaranel <elaranel@gmail.com>
2025-05-09 22:09:27 +0100

    Working through lesson

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"sync/atomic"
+)
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return next
+}
+
+func main() {
+	apiCfg := apiConfig{}
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
